b1fc163ed218e0a09c69095898bb6bd93c0e1242
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2017-06-30 12:02:43 -0400

    added flash match test bin

--- flashandcharge_trackselect/dev/compare_flashmatch.cxx
+++ flashandcharge_trackselect/dev/compare_flashmatch.cxx
@@ -9,10 +9,10 @@ int main( int nargs, char** argv ) {
   // 1) Load information using DataCoordinator
   // 2) Load truth end points for cosmics
   // 3) use the end points to reco the tracks (either linearcharge or astar tracker)
-  // 4) use the reco track points to calculate the charge and charge profile
+  // 4) use the reco track points to calculate the flash hypothesis chi-2
   // 5) make plots comparing the relative values on the planes -- is the distribution good enough?
 
-  // 6) make reco end points. repeat exercise.  But we can now compare charge matching for good and bad tracks
+  // 6) make reco end points. repeat exercise.  But we can now compare flash-matching for good and bad tracks
   
   return 0;
 }
