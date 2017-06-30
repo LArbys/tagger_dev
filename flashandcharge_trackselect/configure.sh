9286b93c9e92bf87b33ec237494545839c4f7449
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2017-06-30 11:30:16 -0400

    added flashcharge selection dev folder

---
+++ flashandcharge_trackselect/configure.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -z ${FQ_TAGGERDEV_BASEDIR+x} ]; then
+    export FQ_TAGGERDEV_BASEDIR=$PWD
+fi
+
+# setup environment variables
+source $FQ_TAGGERDEV_BASEDIR/setup.sh
+
+# setup larlite
+source $FQ_TAGGERDEV_BASEDIR/larlite/config/setup.sh
+
+# setup laropencv
+#source $FQ_TAGGERDEV_BASEDIR/LArOpenCV/setup_laropencv.sh
+
+# setup Geo2D
+#source $FQ_TAGGERDEV_BASEDIR/Geo2D/config/setup.sh
+
+# setup LArCV
+source $FQ_TAGGERDEV_BASEDIR/LArCV/configure.sh
+
+# setup larlitecv
+source $FQ_TAGGERDEV_BASEDIR/larlitecv/configure.sh
+
---
+++ flashandcharge_trackselect/dev/compare_trackq.cxx
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main( int nargs, char** argv ) {
+  
+  std::cout << "COMPARE TRACK CHARGE" << std::endl;
+  
+  return 0;
+}
